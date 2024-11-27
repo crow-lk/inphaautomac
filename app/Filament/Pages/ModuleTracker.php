77ecc545f26f29b27b561688f76335724fb25a8d
Kavindu Lakshitha <kaviya@Mac.lan>
2024-11-28 02:47:38 +0530

    minor arrangments.

--- app/Filament/Pages/ModuleTracker.php
+++ app/Filament/Pages/ModuleTracker.php
@@ -8,5 +8,10 @@ class ModuleTracker extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
+    protected static ?string $navigationGroup = 'Inpha BMS';
+
+    //move this group next to Registrations
+    protected static ?int $navigationSort = 2;
+
     protected static string $view = 'filament.pages.module-tracker';
 }
