e88554fd1e53dfcdd9a2b10807218b8fe3df1b7b
Kavindu Lakshitha <kavindu.lakshitha.ofc@gmail.com>
2024-12-21 02:05:21 +0530

    Duplicate counts bug fix for the widgets.

--- app/Filament/Resources/ModuleResource/Widgets/ModulesOverview.php
+++ app/Filament/Resources/ModuleResource/Widgets/ModulesOverview.php
@@ -11,44 +11,102 @@ class ModulesOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total Modules', Module::distinct('serial_number')->count('serial_number')),
+            Stat::make('Total Modules', Module::whereIn('id', function ($query) {
+                $query->selectRaw('MAX(id) AS id')
+                    ->from('modules')
+                    ->groupBy('serial_number');
+            })->count()),
             //group according to the capacity & ir value
             Stat::make('Grade A Modules', Module::whereBetween('capacitance', [4000, 6000])
-                ->distinct('serial_number')
-                ->count('serial_number'))
+                ->whereIn('id', function ($query) {
+                    $query->selectRaw('MAX(id) AS id')
+                        ->from('modules')
+                        ->whereNotIn('battery_pack_id', function ($subQuery) {
+                            $subQuery->select('id')
+                                ->from('battery_packs')
+                                ->where('name', 'LIKE', 'CINU%'); // Exclude customer-issued battery packs
+                        })
+                        ->groupBy('serial_number');
+                })->count())
                 ->description("Capacity between 4000-6000mAh")
                 ->descriptionIcon('heroicon-s-battery-100')
                 ->color('success'),
+
             Stat::make('Grade B Modules', Module::whereBetween('capacitance', [3000, 4000])
-                ->distinct('serial_number')
-                ->count('serial_number'))
+                ->whereIn('id', function ($query) {
+                    $query->selectRaw('MAX(id) AS id')
+                        ->from('modules')
+                        ->whereNotIn('battery_pack_id', function ($subQuery) {
+                            $subQuery->select('id')
+                                ->from('battery_packs')
+                                ->where('name', 'LIKE', 'CINU%');
+                        })
+                        ->groupBy('serial_number');
+                })->count())
                 ->description("Capacity between 3000-4000mAh")
                 ->descriptionIcon('heroicon-s-battery-50')
                 ->color('primary'),
+
             Stat::make('Grade C Modules', Module::whereBetween('capacitance', [2000, 3000])
-                ->distinct('serial_number')
-                ->count('serial_number'))
+                ->whereIn('id', function ($query) {
+                    $query->selectRaw('MAX(id) AS id')
+                        ->from('modules')
+                        ->whereNotIn('battery_pack_id', function ($subQuery) {
+                            $subQuery->select('id')
+                                ->from('battery_packs')
+                                ->where('name', 'LIKE', 'CINU%');
+                        })
+                        ->groupBy('serial_number');
+                })->count())
                 ->description("Capacity between 2000-3000mAh")
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('info'),
+
             Stat::make('Grade D Modules', Module::whereBetween('capacitance', [1000, 2000])
-                ->distinct('serial_number')
-                ->count('serial_number'))
+                ->whereIn('id', function ($query) {
+                    $query->selectRaw('MAX(id) AS id')
+                        ->from('modules')
+                        ->whereNotIn('battery_pack_id', function ($subQuery) {
+                            $subQuery->select('id')
+                                ->from('battery_packs')
+                                ->where('name', 'LIKE', 'CINU%');
+                        })
+                        ->groupBy('serial_number');
+                })->count())
                 ->description("Capacity between 1000-2000mAh")
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('warning'),
-            Stat::make('Grade E Modules', Module::whereBetween('capacitance',[0, 1000])
-                ->distinct('serial_number')
-                ->count('serial_number'))
+
+            Stat::make('Grade E Modules', Module::where('capacitance', '<', 1000)
+                ->whereIn('id', function ($query) {
+                    $query->selectRaw('MAX(id) AS id')
+                        ->from('modules')
+                        ->whereNotIn('battery_pack_id', function ($subQuery) {
+                            $subQuery->select('id')
+                                ->from('battery_packs')
+                                ->where('name', 'LIKE', 'CINU%');
+                        })
+                        ->groupBy('serial_number');
+                })->count())
                 ->description("Capacity less than 1000mAh")
                 ->descriptionIcon('heroicon-s-battery-0')
                 ->color('danger'),
+
             Stat::make('N/A Modules', Module::whereNull('capacitance')
-                ->distinct('serial_number')
-                ->count('serial_number'))
+                ->whereIn('id', function ($query) {
+                    $query->selectRaw('MAX(id) AS id')
+                        ->from('modules')
+                        ->whereNotIn('battery_pack_id', function ($subQuery) {
+                            $subQuery->select('id')
+                                ->from('battery_packs')
+                                ->where('name', 'LIKE', 'CINU%');
+                        })
+                        ->groupBy('serial_number');
+                })->count())
                 ->description("Capacitance not available")
                 ->descriptionIcon('heroicon-m-information-circle')
                 ->color('gray'),
+
         ];
     }
 }
