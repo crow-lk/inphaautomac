b81735e90e56405c844940fd5cedabd40fcdf9af
MalinduMax <malindumaxxa0@gmail.com>
2024-12-26 17:08:19 +0530

    Added font files and inproved invoice design

---
+++ config/dompdf.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'fontDir' => [
+        public_path('fonts/OpenSans'),
+        // other directories...
+    ],
+
+    'fontMetrics' => [
+        'Open Sans' => [
+            'normal' => 'OpenSans-Regular.ttf',
+            'bold' => 'OpenSans-Bold.ttf',
+            'italic' => 'OpenSans-Italic.ttf',
+            'bold_italic' => 'OpenSans-BoldItalic.ttf',
+        ],
+    ],
+];
--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -25,26 +25,33 @@
     <table>
         <thead>
             <tr>
-                <th style="width:40%;">Description</th>
-                <th style="width:20%;">Quantity</th>
-                <th style="width:20%;">Price (Rs.)</th>
-                <th style="width:20%;">Total (Rs.)</th>
+                <th style="width:10%; text-align: center;">NO</th>
+                <th style="width:70%; text-align: left;">DESCRIPTION</th>
+                <th style="width:20%; text-align: center;">PRICE (Rs.)</th>
             </tr>
         </thead>
         <tbody>
             @foreach($invoice->invoiceItems as $item)
                 <tr>
-                    <td style="width:40%;">{{ $item->description }}</td>
-                    <td style="width:20%;">{{ $item->quantity }}</td>
-                    <td style="width:20%;">{{ number_format($item->price, 2) }}</td>
-                    <td style="width:20%;">{{ number_format($item->quantity * $item->price, 2) }}</td>
+                    <td style="width:10%; text-align: center;">{{ $item->id }}</td>
+                    <td style="width:70%; text-align: left;">{{ $item->description }}</td>
+                    <td style="width:20%; text-align: right;">{{ number_format($item->price, 2) }}</td>
                 </tr>
             @endforeach
         </tbody>
     </table>
     <div class="new">
-        <p class="total">Total Amount:  Rs.{{ number_format($invoice->amount, 2) }}</p>
+        <p class="total">GRAND TOTAL:  Rs.{{ number_format($invoice->amount, 2) }}</p>
     </div>
-
+    <div class="text1">
+        <p class="h1"><strong>Inpha Auto Mac & Hybrid Care</strong></p>
+        <p class="p1"><strong>Thank you for buisness with us!</strong></p>
+    </div>
+    <div class="text2">
+        <p><strong>Term and Conditions :</strong></p>
+        <p class="p1">It is mandatory to bring the invoice given to you</p>
+        <p class="p1">for any Battery, ABS related services.</p>
+        <p class="p1">Physical damages are not covered under warranty.</p>
+    </div>
 </body>
 </html>
